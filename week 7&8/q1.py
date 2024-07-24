769b40e9eb9fe8525e6f61f6abd97df3622e1d58
Anushka <itsanushkaonly13@gmail.com>
2024-07-25 00:16:49 +0530

    Added week 7&8

--- week 7&8/q1.py
+++ week 7&8/q1.py
@@ -2,10 +2,7 @@ import json
 import copy  # use it for deepcopy if needed
 import math  # for math.inf
 import logging
-import pygame
-
-pygame.init()
-
+import time
 
 
 logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
@@ -15,6 +12,7 @@ logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', datefmt=
 # Mapping from histories (str) to probability distribution over actions
 strategy_dict_x = {}
 strategy_dict_o = {}
+utilities = {}
 
 
 class History:
@@ -56,7 +54,10 @@ class History:
         Get player whose turn it is at the current history/board
         :return: 'x' or 'o' or None
         """
-        total_num_moves = len(self.history)
+        if self.history is None:
+            total_num_moves = 0
+        else:
+            total_num_moves = len(self.history)
         if total_num_moves < 9:
             if total_num_moves % 2 == 0:
                 return 'x'
@@ -81,73 +82,110 @@ class History:
     def is_win(self):
         # check if the board position is a win for either players
         # Feel free to implement this in anyway if needed
-        combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-        for comb in combinations:
-            if self.board[comb[0]] == self.board[comb[1]] == self.board[comb[2]] != '0':
-                return True
-        return False
-        # pass
-        
+        board = self.get_board()
+        if board[0] == board[1] == board[2] == 'x':
+            return 'x'
+        if board[3] == board[4] == board[5] == 'x':
+            return 'x'
+        if board[6] == board[7] == board[8] == 'x':
+            return 'x'
+        if board[0] == board[3] == board[6] == 'x':
+            return 'x'
+        if board[1] == board[4] == board[7] == 'x':
+            return 'x'
+        if board[2] == board[5] == board[8] == 'x':
+            return 'x'
+        if board[0] == board[4] == board[8] == 'x':
+            return 'x'
+        if board[2] == board[4] == board[6] == 'x':
+            return 'x'
+        if board[0] == board[1] == board[2] == 'o':
+            return 'o'
+        if board[3] == board[4] == board[5] == 'o':
+            return 'o'
+        if board[6] == board[7] == board[8] == 'o':
+            return 'o'
+        if board[0] == board[3] == board[6] == 'o':
+            return 'o'
+        if board[1] == board[4] == board[7] == 'o':
+            return 'o'
+        if board[2] == board[5] == board[8] == 'o':
+            return 'o'
+        if board[0] == board[4] == board[8] == 'o':
+            return 'o'
+        if board[2] == board[4] == board[6] == 'o':
+            return 'o'
+        return None
 
     def is_draw(self):
         # check if the board position is a draw
         # Feel free to implement this in anyway if needed
-        if '0' not in self.board:
-            if not self.is_win():
-                return True
+        # print(self.get_board())
+        if ('0' not in self.get_board()) and (self.is_win() is None):
+            return True
         return False
-        # pass
-
 
     def get_valid_actions(self):
         # get the empty squares from the board
         # Feel free to implement this in anyway if needed
         valid_actions = []
         for i in range(9):
-            if self.board[i] == '0':
+            if i not in self.history:
                 valid_actions.append(i)
         return valid_actions
-        # pass
 
     def is_terminal_history(self):
         # check if the history is a terminal history
         # Feel free to implement this in anyway if needed
-        if self.is_win() or self.is_draw():
+        # print(f'{self.history} Is_win: {self.is_win()} Is_draw: {self.is_draw()}')
+        if self.is_win() is not None or self.is_draw():
+            self.get_utility_given_terminal_history()
             return True
         return False
-        # pass
 
     def get_utility_given_terminal_history(self):
         # Feel free to implement this in anyway if needed
-        if self.is_win():
-            if self.player == 'x':
-                return 1
-            else:
-                return -1
-        else:
+        global utilities
+        if self.is_win() == 'x':
+            utilities[self.get_board_string()] = (1, None)
+            return 1
+        elif self.is_win() == 'o':
+            utilities[self.get_board_string()] = (-1, None)
+            return -1
+        elif self.is_draw():
+            utilities[self.get_board_string()] = (0, None)
             return 0
-        # pass
+        return  
         
-
     def update_history(self, action):
         # In case you need to create a deepcopy and update the history obj to get the next history object.
         # Feel free to implement this in anyway if needed
-        new_History = copy.deepcopy(self)
-        new_History.history.append(action)
-        new_History.board = new_History.get_board()
-        new_History.player = new_History.current_player()
-        return new_History
-        # pass
-
-strategy_dict_x = {}
-strategy_dict_o = {}
+        new_history = History()
+        new_history.history = copy.deepcopy(self.history)
+        new_history.history.append(action)
+        return new_history
+    
+    def get_board_string(self):
+        return ''.join(self.get_board())
+    
+    def __str__(self):
+        hiSTRy = ''
+        for element in self.history:
+            hiSTRy += str(element)
+        return hiSTRy
+    
+def get_dict(action):
+    prob_dist = dict()
+    for i in range(9):
+        prob_dist[str(i)] = int(action == i)
+    return prob_dist
 
 def backward_induction(history_obj):
     """
     :param history_obj: Histroy class object
     :return: best achievable utility (float) for th current history_obj
     """
-    # global strategy_dict_x, strategy_dict_o
+    global strategy_dict_x, strategy_dict_o
     # TODO implement
     # (1) Implement backward induction for tictactoe
     # (2) Update the global variables strategy_dict_x or strategy_dict_o which are a mapping from histories to
@@ -161,54 +199,151 @@ def backward_induction(history_obj):
     # actions. But since tictactoe is a PIEFG, there always exists an optimal deterministic strategy (SPNE). So your
     # policy will be something like this {"0": 1, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0} where
     # "0" was the one of the best actions for the current player/history.
-    board_str = ''.join([str(history_obj.history[i]) for i in range(len(history_obj.history))])
-    action_dict = {"0":0, "1":0, "2":0, "3":0, "4":0, "5":0, "6":0, "7":0, "8":0}
-    
-    if history_obj.is_terminal_history():
-        return history_obj.get_utility_given_terminal_history()
-    
-    if history_obj.player == 'x' and board_str in strategy_dict_x:
-        return strategy_dict_x[board_str]
-    
-    if history_obj.player == 'o' and board_str in strategy_dict_o:
-        return strategy_dict_o[board_str]
-    
-    if history_obj.player == 'x':
-        v = -math.inf
-        best_action = None
-        for action in history_obj.get_valid_actions():
-            new_history = history_obj.update_history(action)
-            new_v = backward_induction(new_history)
-            if new_v > v:
-                v = new_v
-                best_action = str(action)
-                action_dict[best_action] = 1
-        strategy_dict_x[board_str] = action_dict
-        return v
-    
-    if history_obj.player == 'o':
-        v = math.inf
-        best_action = None
-        for action in history_obj.get_valid_actions():
-            new_history = history_obj.update_history(action)
-            new_v = backward_induction(new_history)
-            if new_v < v:
-                v = new_v
-                best_action = str(action)
-                action_dict[best_action] = 1
-        strategy_dict_o[board_str] = action_dict
-        return v
-    
-    
+    if not history_obj.is_terminal_history():
+        # if str(history_obj) == '0412':
+        #     print(history_obj.get_valid_actions())
+        if history_obj.current_player() == 'x':
+            best_utility = -2
+            best_move = -1
+            # print(history_obj.get_valid_actions())
+            # time.sleep(1)
+            for move in history_obj.get_valid_actions():
+                child = history_obj.update_history(move)
+                # print(str(child))
+                board_string = child.get_board_string()
+                # if str(child)[:-1] == '0412':
+                #     print(str(child), board_string in utilities, utilities[board_string])
+                if board_string in utilities:
+                    child_utility, cmove = utilities[board_string]
+                #     if cmove is not None:
+                #         strategy_dict_o[str(child)] = get_dict(move)
+                else:
+                    child_utility = backward_induction(child)
+                if child_utility > best_utility:
+                    best_move = move    
+                    best_utility = child_utility
+            strategy_dict_x[str(history_obj)] = get_dict(best_move)
+        else:
+            best_utility = 2
+            best_move = -1
+            for move in history_obj.get_valid_actions():
+                child = history_obj.update_history(move)
+                board_string = child.get_board_string()
+                if str(child) in utilities:
+                    child_utility, cmove = utilities[board_string]
+                #     if cmove is not None:
+                #         strategy_dict_x[str(child)] = get_dict(move)
+                else:
+                    child_utility = backward_induction(child)
+                # print(child_utility)
+                if child_utility < best_utility:
+                    best_move = move
+                    best_utility = child_utility
+            # print('best', best_move)
+            strategy_dict_o[str(history_obj)] = get_dict(best_move)
+        utilities[history_obj.get_board_string()] = (best_utility, best_move)
+    else:
+        best_utility = utilities[history_obj.get_board_string()][0]
+        # print(best_utility)
+    return best_utility
     # TODO implement
+
+# import itertools
+
+
+def are_permutations(history, other):
+    odd_elements_history = set()
+    even_elements_history = set()
+    odd_elements_other = set()
+    even_elements_other = set()
+
+    for i in range(len(history)):
+        if i % 2 == 0:
+            even_elements_history.add(history[i])
+            even_elements_other.add(other[i])
+        else:
+            odd_elements_history.add(history[i])
+            odd_elements_other.add(other[i])
+
+    if odd_elements_history == odd_elements_other and even_elements_history == even_elements_other:
+        return True
+    return False
+
+permutations = dict()
+
+function_count = 0
+
+def get_permutations(history):
+    global permutations, function_count
+
+    function_count += 1
+    length = len(history)
+    odd_elements = []
+    even_elements = []
+    # print(history, length)
+    if length == 1 or length == 2:
+        # print('Returning')
+        permutations[history] = set([history,])
+        return set([history,])
+        # yield history
+    
+    if history in permutations:
+        return permutations[history]
+
+    for i in range(length):
+        if i % 2 == 0:
+            even_elements.append(history[i])
+        else:
+            odd_elements.append(history[i])
+
+    permutations[history] = set()
+
+    for i in range(0, length, 2):
+        for j in range(1, length, 2):
+            dupe = history
+            if i < j:
+                dupe = dupe[:i] + dupe[0] + dupe[i+1:j] + dupe[1] + dupe[j+1:]
+            else:
+                dupe = dupe[:j] + dupe[1] + dupe[j+1:i] + dupe[0] + dupe[i+1:]
+            dupe = dupe[2:]
+            # perms = get_permutations(dupe)
+            # print(history, dupe, list(perms))
+            # time.sleep(1)
+            # print(dupe, get_permutations(dupe))
+            for perm in get_permutations(dupe):
+                # print('Yielding', history[i], history[j], perm)
+                # yield history[i]+history[j]+perm
+                permutations[history].add(history[i]+history[j]+perm)
+    return permutations[history]
+
 
 
 def solve_tictactoe():
+    global strategy_dict_x, strategy_dict_o, utilities, permutations, function_count
+    start_time = time.time()
     backward_induction(History())
+    logging.info('Break')
+    print(time.time() - start_time)
+    x_keys = list(strategy_dict_x.keys())
+    for element in x_keys:
+        element_prob = strategy_dict_x[element]
+        for emelent in get_permutations(element):
+            strategy_dict_x[emelent] = element_prob
+
+    o_keys = list(strategy_dict_o.keys())
+    for element in o_keys:
+        element_prob = strategy_dict_o[element]
+        for emelent in get_permutations(element):
+            strategy_dict_o[emelent] = element_prob
+    
     with open('./policy_x.json', 'w') as f:
         json.dump(strategy_dict_x, f)
     with open('./policy_o.json', 'w') as f:
         json.dump(strategy_dict_o, f)
+    # print(len(strategy_dict_o), len(strategy_dict_x))
+    # print(permutations)
+    # print(function_count)
+    print(time.time() - start_time)
     return strategy_dict_x, strategy_dict_o
 
 
